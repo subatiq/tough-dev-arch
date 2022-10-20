b1f7681b280516c44353a2ebf5e92dd02b88da41
Vladimir Semenov <sepsseven@gmail.com>
2022-10-20 18:57:41 +0300

    Add analytics service

--- analytics/src/task/model.py
+++ analytics/src/task/model.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from uuid import UUID, uuid4
 from enum import Enum
 from pydantic import BaseModel, Field
@@ -10,11 +11,13 @@ class TaskStatus(Enum):
 
 class Task(BaseModel):
     id: UUID = Field(default_factory=uuid4)
-    pub_id: UUID = Field(default_factory=uuid4)
+    pub_id: UUID
+    created_at: datetime
+    completed_at: datetime
     title: str
     description: str
     assignee: UUID
-    status: TaskStatus = TaskStatus.IN_PROGRESS
+    status: TaskStatus
 
 
 
